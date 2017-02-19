94b9ad55ba8f96d7ac98093b856bee786d960576
invertisment <visiskaj.slaptaj@gmail.com>
2017-02-19 22:03:48 +0200

    word chains. non brute force impl WIP #82

--- spec/core/word_chains_spec.clj
+++ spec/core/word_chains_spec.clj
@@ -14,6 +14,11 @@
               (a/lev
                 "test"
                 "test1")))
+ (it "should count lev distance 1"
+     (should= 1
+              (a/lev
+                "test"
+                "tes1")))
  (it "should count lev distance 2"
      (should= 2
               (a/lev
@@ -23,7 +28,27 @@
      (should= 2
               (a/lev
                 "b"
-                "abc"))))
+                "abc")))
+ (it "should count lev distance 4"
+     (should= 4
+              (a/lev
+                "Hello"
+                "World")))
+ (it "should count lev distance 5"
+     (should= 5
+              (a/lev
+                "Hello"
+                "")))
+ (it "should count lev distance 2"
+     (should= 2
+              (a/lev
+                "hat"
+                "coat")))
+ (it "should count lev distance 2"
+     (should= 2
+              (a/lev
+                "stop"
+                "tops"))))
 
 (describe
   "path finder"
@@ -35,7 +60,49 @@
      (should= true (a/path #{"a" "ac" "abc"}))))
 
 (describe
+  "adjacent finder"
+  (it "should find adjacent node set no gap"
+     (should= '([1 0][2 0][0 1][2 1][0 2][1 2]) (a/find-adjacent #{"a" "b" "c"})))
+  (it "should find adjacent node set gap"
+     (should= '([2 0][0 2]) (a/find-adjacent #{"a" "bb" "c"}))))
+
+; TODO: use adjacent finder to with Hamilton's path
+
+(describe
   "4clojure tests"
-  (it "should pass 1"
-      (should= true (a/path #{"hat" "coat" "dog" "cat" "oat" "cot" "hot" "hog"}))
-  ))
+;  (it "should pass 1" (should= true (a/path #{"hat" "coat" "dog" "cat" "oat" "cot" "hot" "hog"})))
+;  (it "should pass 2" (should= false (a/path #{"cot" "hot" "bat" "fat"})))
+;  (it "should pass 3" (should= false (a/path #{"to" "top" "stop" "tops" "toss"})))
+;  (it "should pass 4" (should= true (a/path #{"spout" "do" "pot" "pout" "spot" "dot"})))
+;  (it "should pass 5" (should= true (a/path #{"share" "hares" "shares" "hare" "are"})))
+;  (it "should pass 6" (should= false (a/path #{"share" "hares" "hare" "are"})))
+
+
+
+
+;  (it "should pass 1"
+;      (should= true (for [a '("hat" "coat" "dog" "cat" "oat" "cot" "hot" "hog")
+;                          b '("hat" "coat" "dog" "cat" "oat" "cot" "hot" "hog")]
+;                      (println [a b (a/lev a b)])
+;                      )))
+  )
+
+(describe
+  "distances test"
+;  (it "should return false on certain"
+;      (should= '((0 4 2)
+;                 (4 0 5)
+;                 (2 5 0))
+;               (a/distances '("hello" "World" "yellow"))) )
+;  (it "should find distances"
+;      (should= '((0 4 2)
+;                 (4 0 5)
+;                 (2 5 0))
+;               (a/distances '("hello" "World" "yellow"))) )
+;  (it "should find distances"
+;      (should= '((0 4 2)
+;                 (4 0 5)
+;                 (2 5 0))
+;               (a/distances #{"hat" "coat" "dog" "cat" "oat" "cot" "hot" "hog"})))
+  )
+
--- src/core/word_chains.clj
+++ src/core/word_chains.clj
@@ -25,28 +25,48 @@
               (- i2 1))
             (if
               (=
-               (nth w1 i1)
-               (nth w2 i2))
+               (nth w1 (- i1 1))
+               (nth w2 (- i2 1)))
               0
               1)) )))]
     (-lev
-      (- (count w1) 1)
-      (- (count w2) 1))))
+      (count w1)
+      (count w2))))
 
 (defn path [li]
   (if
    (< (count li) 2)
    true
-   ;(reduce
-   ; (fn [b1 b2]
-;      (or b1 b2))
-    (for [word li
-          :let [others (remove #{word} li)]
-          other others]
-      (if
-        (< (lev word other) 2)
-        (path others)
-        false)))
-)
-;)
+   (reduce
+     (fn [b1 b2]
+       (or b1 b2))
+     (for [word li
+           :let [others (remove #{word} li)]
+           other others]
+       (if
+         (< (lev word other) 2)
+         (path others)
+         false)) )))
 
+(defn distances [li]
+  (partition
+    (count li)
+    (for [x li
+          y li]
+      (lev x y)) ))
+
+(defn find-adjacent [li]
+  (map
+   second
+   (apply concat
+           (map-indexed
+             (fn [y-pos y-li]
+               (filter
+                 (fn [item]
+                   (= (nth item 0) 1))
+                 (map-indexed
+                   (fn [x-pos item]
+                     [item [x-pos y-pos]])
+                   y-li)))
+             (distances li))) )
+)
