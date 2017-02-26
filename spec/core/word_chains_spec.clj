a087986bd7e370e1fcdfddb04beb41cf298702d1
invertisment <visiskaj.slaptaj@gmail.com>
2017-02-26 14:43:22 +0200

    #82 Word chains complete

--- spec/core/word_chains_spec.clj
+++ spec/core/word_chains_spec.clj
@@ -3,106 +3,139 @@
             [core.word-chains :as a]))
 
 (describe
- "lev distance"
- (it "should find happy path"
-     (should= 0
-              (a/lev
-                "test"
-                "test")))
- (it "should count lev distance 1"
-     (should= 1
-              (a/lev
-                "test"
-                "test1")))
- (it "should count lev distance 1"
-     (should= 1
-              (a/lev
-                "test"
-                "tes1")))
- (it "should count lev distance 2"
-     (should= 2
-              (a/lev
-                "test"
-                "toot")))
- (it "should count lev distance 2"
-     (should= 2
-              (a/lev
-                "b"
-                "abc")))
- (it "should count lev distance 4"
-     (should= 4
-              (a/lev
-                "Hello"
-                "World")))
- (it "should count lev distance 5"
-     (should= 5
-              (a/lev
-                "Hello"
-                "")))
- (it "should count lev distance 2"
-     (should= 2
-              (a/lev
-                "hat"
-                "coat")))
- (it "should count lev distance 2"
-     (should= 2
-              (a/lev
-                "stop"
-                "tops"))))
-
-(describe
-  "path finder"
-  (it "should find path through simple text"
-     (should= true (a/path #{"a" "b" "c"})))
-  (it "should not find path through changing text"
-     (should= false (a/path #{"a" "b" "abc"})))
-  (it "should find path through changing text"
-     (should= true (a/path #{"a" "ac" "abc"}))))
+  "lev distance"
+  (it "should find happy path"
+      (should= 0
+               (a/lev
+                 "test"
+                 "test")))
+  (it "should count lev distance 1"
+      (should= 1
+               (a/lev
+                 "test"
+                 "test1")))
+  (it "should count lev distance 1"
+      (should= 1
+               (a/lev
+                 "test"
+                 "tes1")))
+  (it "should count lev distance 2"
+      (should= 2
+               (a/lev
+                 "test"
+                 "toot")))
+  (it "should count lev distance 2"
+      (should= 2
+               (a/lev
+                 "b"
+                 "abc")))
+  (it "should count lev distance 4"
+      (should= 4
+               (a/lev
+                 "Hello"
+                 "World")))
+  (it "should count lev distance 5"
+      (should= 5
+               (a/lev
+                 "Hello"
+                 "")))
+  (it "should count lev distance 2"
+      (should= 2
+               (a/lev
+                 "hat"
+                 "coat")))
+  (it "should count lev distance 2"
+      (should= 2
+               (a/lev
+                 "stop"
+                 "tops"))))
 
 (describe
   "adjacent finder"
   (it "should find adjacent node set no gap"
-     (should= '([1 0][2 0][0 1][2 1][0 2][1 2]) (a/find-adjacent #{"a" "b" "c"})))
+      (should= #{#{0 1}#{0 2}#{1 2}} (a/find-filtered-adjacents #{"a" "b" "c"})))
   (it "should find adjacent node set gap"
-     (should= '([2 0][0 2]) (a/find-adjacent #{"a" "bb" "c"}))))
+      (should= #{#{0 2}} (a/find-filtered-adjacents #{"a" "bb" "c"})))
+  (it "should find adjacent node non-connected"
+      (should= #{#{0 1}#{2 3}} (a/find-filtered-adjacents #{"cot" "hot" "bat" "fat"}))))
 
-; TODO: use adjacent finder to with Hamilton's path
+(describe
+  "single connection walker"
+  (it
+    "should not walk on empty"
+    (should=
+      false
+      (a/walk-by-single-connection (list) [#{1 0}] '())))
+  (it
+    "should be true on empty node list"
+    (should=
+      true
+      (a/walk-by-single-connection (list 1) [] '())))
+  (it
+    "should walk connected"
+    (should=
+      true
+      (a/walk-by-single-connection (list 1) [#{1 0}] '())))
+  (it
+    "should walk non connected"
+    (should=
+      false
+      (a/walk-by-single-connection (list 3) [#{1 0}] '())))
+  (it
+    "should walk non connected"
+    (should=
+      false
+      (a/walk-by-single-connection (list 3) [#{3 0} #{1 2}] '())))
+  (it
+    "should walk connected 1"
+    (should=
+      true
+      (a/walk-by-single-connection (list 3) [#{0 1} #{3 2} #{1 2}] '())))
+  (it
+    "should not ignore non-matching node"
+    (should=
+      false
+      (a/walk-by-single-connection (list 3) [#{0 4} #{3 2} #{1 2}] '())))
+  (it
+    "should handle loop"
+    (should=
+      true
+      (a/walk-by-single-connection (list :a) [#{:a :b} #{:b :c} #{:c :d} #{:d :e} #{:b :f} #{:c :g} #{:d :g} #{:f :g} #{:f :h} #{:g :h}] '())))
+  (it
+    "should handle connected adjacent crossing"
+    (should=
+      false
+      (a/walk-by-single-connection (list :b) [#{:a :b} #{:b :c} #{:b :d}] '())))
+  (it
+    "should handle connected adjacent crossing with middle node"
+    (should=
+      false
+      (a/walk-by-single-connection (list :b) [#{:a :b} #{:b :c} #{:c :d} #{:e :d} #{:f :d}] '()))))
 
 (describe
   "4clojure tests"
-;  (it "should pass 1" (should= true (a/path #{"hat" "coat" "dog" "cat" "oat" "cot" "hot" "hog"})))
-;  (it "should pass 2" (should= false (a/path #{"cot" "hot" "bat" "fat"})))
-;  (it "should pass 3" (should= false (a/path #{"to" "top" "stop" "tops" "toss"})))
-;  (it "should pass 4" (should= true (a/path #{"spout" "do" "pot" "pout" "spot" "dot"})))
-;  (it "should pass 5" (should= true (a/path #{"share" "hares" "shares" "hare" "are"})))
-;  (it "should pass 6" (should= false (a/path #{"share" "hares" "hare" "are"})))
-
-
-
-
-;  (it "should pass 1"
-;      (should= true (for [a '("hat" "coat" "dog" "cat" "oat" "cot" "hot" "hog")
-;                          b '("hat" "coat" "dog" "cat" "oat" "cot" "hot" "hog")]
-;                      (println [a b (a/lev a b)])
-;                      )))
-  )
-
-(describe
-  "distances test"
-;  (it "should return false on certain"
-;      (should= '((0 4 2)
-;                 (4 0 5)
-;                 (2 5 0))
-;               (a/distances '("hello" "World" "yellow"))) )
-;  (it "should find distances"
-;      (should= '((0 4 2)
-;                 (4 0 5)
-;                 (2 5 0))
-;               (a/distances '("hello" "World" "yellow"))) )
-;  (it "should find distances"
-;      (should= '((0 4 2)
-;                 (4 0 5)
-;                 (2 5 0))
-;               (a/distances #{"hat" "coat" "dog" "cat" "oat" "cot" "hot" "hog"})))
-  )
+  (it "should pass 1"
+      (should=
+        true
+        (a/path #{"hat" "coat" "dog" "cat" "oat" "cot" "hot" "hog"})))
+  (it "should pass 2"
+      (should=
+        false
+        (a/path #{"cot" "hot" "bat" "fat"})))
+  (it "should pass 3"
+      (should=
+        false
+        (a/path #{"to" "top" "stop" "tops" "toss"})))
+  (it "should pass 4"
+      (should=
+        true
+        (a/path #{"spout" "do" "pot" "pout" "spot" "dot"})))
+  (it "should pass 5"
+      (should=
+        true
+        (a/path #{"share" "hares" "shares" "hare" "are"})))
+  (it "should pass 6"
+      (should=
+        false
+        (a/path #{"share" "hares" "hare" "are"}))))
 
--- src/core/word_chains.clj
+++ src/core/word_chains.clj
@@ -33,21 +33,6 @@
       (count w1)
       (count w2))))
 
-(defn path [li]
-  (if
-   (< (count li) 2)
-   true
-   (reduce
-     (fn [b1 b2]
-       (or b1 b2))
-     (for [word li
-           :let [others (remove #{word} li)]
-           other others]
-       (if
-         (< (lev word other) 2)
-         (path others)
-         false)) )))
-
 (defn distances [li]
   (partition
     (count li)
@@ -58,15 +43,74 @@
 (defn find-adjacent [li]
   (map
    second
-   (apply concat
+   (apply
+     concat
+     (map-indexed
+       (fn [y-pos y-li]
+         (filter
+           (fn [item]
+             (= (nth item 0) 1))
            (map-indexed
-             (fn [y-pos y-li]
-               (filter
-                 (fn [item]
-                   (= (nth item 0) 1))
-                 (map-indexed
-                   (fn [x-pos item]
-                     [item [x-pos y-pos]])
-                   y-li)))
-             (distances li))) )
-)
+             (fn [x-pos item]
+               [item [x-pos y-pos]])
+             y-li)))
+       (distances li)))))
+
+(defn filter-adjacent [adj]
+  (set (map set adj)))
+
+(defn find-filtered-adjacents [li]
+  (filter-adjacent (find-adjacent li)))
+
+(defn walk-by-single-connection
+  ([vertexes-to-check nodes breadcrumb]
+   #_(println "to-check:" vertexes-to-check "breadcrumb:" breadcrumb "nodes:" nodes)
+   (if
+     (empty? vertexes-to-check)
+     false
+     (if
+       (empty? nodes)
+       true
+       (let
+         [[vertex vertexes-rest] ((juxt first rest) vertexes-to-check)
+          #_(println "vertex" vertex)]
+         (if
+           (loop [node-list nodes]
+             (if
+               (empty? node-list)
+               false
+               (let [[current-node rest-nodes] ((juxt first rest) node-list)
+                     deep (if
+                            (contains? current-node vertex)
+                            (walk-by-single-connection
+                              (remove #{vertex} current-node)
+                              (filter
+                                (fn [to-filter]
+                                  #_(println to-filter vertex)
+                                  (not (= 0
+                                          (count (remove
+                                                   (set (conj breadcrumb vertex))
+                                                   to-filter)))))
+                                (remove #{current-node} nodes))
+                              (conj breadcrumb vertex))
+                            false)]
+                 #_(println "recur" current-node rest-nodes)
+                 (or
+                   deep
+                   (recur rest-nodes)))))
+           true
+           (walk-by-single-connection
+             vertexes-rest
+             nodes
+             breadcrumb)))))))
+
+(defn path [li]
+  (let [adjacents (find-filtered-adjacents li)]
+    (apply
+      walk-by-single-connection
+      ((juxt
+         first
+         identity
+         empty)
+       adjacents))))
+
